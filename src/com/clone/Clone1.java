945666ef5cdecaa10469d8ea65d60a3e742bfd42
vwaran22895 <vwaran22895@gmail.com>
2022-09-24 15:45:33 +0530

    loser

---
+++ src/com/clone/Clone1.java
@@ -0,0 +1,13 @@
+package com.clone;
+
+public class Clone1 {
+	
+	private void hai() {
+		System.out.println("hell0");
+	}
+public static void main(String[] args) {
+	
+	Clone1 c = new Clone1();
+	c.hai();
+}
+}
